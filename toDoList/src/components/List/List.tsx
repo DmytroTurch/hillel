922dd265d52dfb5d80ea79aaf110b96af178eb94
DmytroTurch <dmturj@gmail.com>
2023-11-02 23:04:52 +0200

    feat: add unique keys for tasks based on time

--- toDoList/src/components/List/List.tsx
+++ toDoList/src/components/List/List.tsx
@@ -4,8 +4,9 @@ interface listProps {
     toDoes: Array<string>, 
 }
 const List: React.FC<listProps> = ({toDoes}:listProps) => {
-    console.log("done!")
-    const listOfToDoes = toDoes.map((toDo:string) => <ToDo key={toDo}>{toDo}</ToDo>)
+    const now = new Date (Date.now());
+    const dateKey = now.toJSON()
+    const listOfToDoes = toDoes.map((toDo:string) => <ToDo key={dateKey}>{toDo}</ToDo>)
     return <ul>{listOfToDoes}</ul>
 }
 

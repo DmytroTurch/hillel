e0915b537df5c18111359dc1b4c0f64ce2f996e5
DmytroTurch <dmturj@gmail.com>
2023-11-08 16:34:31 +0200

    feat: delete button

--- toDoList/src/components/List/List.tsx
+++ toDoList/src/components/List/List.tsx
@@ -4,7 +4,7 @@ interface listProps {
     toDoes: Array<string>, 
 }
 const List: React.FC<listProps> = ({toDoes}:listProps) => {
-    const listOfToDoes = toDoes.map((toDo:string) => <ToDo key={toDo[1]}>{toDo[0]}</ToDo>)
+    const listOfToDoes = toDoes.map((toDo:string) => <ToDo key={toDo[1]} id={toDo[1]}>{toDo[0]}</ToDo>)
     return <ul>{listOfToDoes}</ul>
 }
 
--- toDoList/src/components/ToDo/ToDo.tsx
+++ toDoList/src/components/ToDo/ToDo.tsx
@@ -3,9 +3,10 @@ import { ToDoOnEdit } from './ToDoOnEdit/ToDoOnEdit'
 import { EditButton } from '../Buttons/EditButton/EditButton'
 interface toDoProps {
     children: string,
+    id: string,
 }
 
-const ToDo: React.FC<toDoProps> = ({children}:toDoProps) => {
+const ToDo: React.FC<toDoProps> = ({children, id}:toDoProps) => {
     const [editing, setEditing] = useState(false)
     const [content, setContent] = useState(children)
 
@@ -14,7 +15,21 @@ const ToDo: React.FC<toDoProps> = ({children}:toDoProps) => {
         setEditing(newState);
     }
 
+    function handleDelete(e){
+        const toDoArr = JSON.parse(localStorage.getItem('toDoes'));
+        const newArr = JSON.stringify(toDoArr.filter(toDO => !(String(toDO[1]) === e.target.id)))
+        console.log(newArr);
+        localStorage.setItem('toDoes', newArr)
+    }
 
-    return <li>{editing ? <ToDoOnEdit value={content} onChange={(e) => setContent(e.target.value)}/> : content } <EditButton onClick={handleEdit}/></li>
+
+    return <li>{editing ? <ToDoOnEdit value={content} onChange={(e) => setContent(e.target.value)}/> : content } 
+    <EditButton onClick={handleEdit}/> 
+    <button
+        id={id} 
+        onClick={(e) => {handleDelete(e)}} 
+    >
+        Delete
+    </button></li>
 }
 export { ToDo }
